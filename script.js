ca3c9dcfaa8707c60a9f7849ac61e66b12c9b0e8
knotnitin <nitinnathan7@gmail.com>
2023-07-22 07:59:56 -0400

    Added hrefs to project divs

---
+++ script.js
@@ -0,0 +1,13 @@
+const projects = document.querySelectorAll(".project") // All project elements
+projects.forEach(project => {
+    project.addEventListener('click', () => {
+        if(project.id == "platformer"){
+            // console.log("platformer")
+            window.location.href = 'webGL-2d/index.html'
+        }
+        else if(project.id == "soccer"){
+            // console.log("soccer")
+            window.location.href = 'webGL-football/index.html'
+        }
+    })
+})
